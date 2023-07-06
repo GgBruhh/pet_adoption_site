f3db013151cf2c91baed88b4bfed900b6bd26d55
Camryn Scott <camryn1242@yahoo.com>
2023-07-06 17:51:30 -0400

    added homepage

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect, request, render_template, flash, session
-from models import Employee, connect_db, db, Department
-from forms import AddSnackForm, EmployeeForm
+from models import Pet, connect_db, db
+# from forms import 
 
 app = Flask(__name__)
 
@@ -11,3 +11,9 @@ app.config['SECRET_KEY'] = "chickenzarecool21837"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 connect_db(app)
+
+
+@app.route('/')
+def homepage():
+    pets = Pet.query.all()
+    return render_template('home.html', pets=pets)
